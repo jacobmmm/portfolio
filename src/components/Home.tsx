a568adbf1d1e8401c6a0a8fb548a20435a7c2f49
jacobmmm <jacob.42193@gmail.com>
2025-07-23 11:51:06 +1000

    removed scroll down button on the homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,10 +25,10 @@ const Home: React.FC = () => {
         <h1 className="hero-title">Jacob Mammen Jacob</h1>
         <h3 className="hero-subtitle">AND THIS IS MY RESUME</h3>
       </div>
-      <button onClick={scrollToAbout} className="scroll-indicator">
+      {/* <button onClick={scrollToAbout} className="scroll-indicator">
         <span className="scroll-text">Scroll to explore</span>
         <div className="scroll-arrow"></div>
-      </button>
+      </button> */}
     </section>
   );
 };
